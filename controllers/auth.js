2e80540ead5e46b0a1a9c0730ffdede1ff53bf04
saira-gulamali <sguacamole98@gmail.com>
2024-06-25 15:35:01 +0100

    setup basic api user & jobs - no search functionality yet

---
+++ controllers/auth.js
@@ -0,0 +1,93 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      name: user.name,
+      lastName: user.lastName,
+      location: user.location,
+      email: user.email,
+      token,
+    },
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  console.log({ email, password });
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials - no user found");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials - password incorrect");
+  }
+  // compare password
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user: {
+      name: user.name,
+      lastName: user.lastName,
+      location: user.location,
+      email: user.email,
+      token,
+    },
+  });
+};
+
+// const updateUser = async (req, res) => {
+//   console.log({ user: req.user, body: req.body });
+
+//   res.status(200).send();
+// };
+
+const updateUser = async (req, res) => {
+  const { email, name, lastName, location } = req.body;
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError("Please provide all values");
+  }
+  // work around for 'Model 'pre' save' bug
+  // const user = await User.findOneAndUpdate(
+  //   { _id: req.user.userId },
+  //   { email, name, lastName, location },
+  //   { new: true, runValidators: true }
+  // );
+
+  const user = await User.findOne({ _id: req.user.userId });
+
+  if (!user) {
+    throw new BadRequestError("No such user exists in our database");
+  }
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+
+  await user.save();
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+      token,
+    },
+  });
+};
+module.exports = {
+  register,
+  login,
+  updateUser,
+};
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { register, login, updateUser } = require("../controllers/auth");
+const authenticateUser = require("../middleware/authentication");
+
+router.post("/register", register);
+router.post("/login", login);
+router.patch("/updateUser", authenticateUser, updateUser);
+
+module.exports = router;
